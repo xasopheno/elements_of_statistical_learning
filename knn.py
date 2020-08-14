898c4f3a828869bd80f6c274858637477a19c7d1
xasopheno <danny@xasopheno.com>
2020-08-14 11:40:18 -0400

    WIP

--- knn.py
+++ knn.py
@@ -5,9 +5,16 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from sklearn import neighbors, datasets
 
+n_neighbors = 21
+n_classes = 2
+
 
 X, y = make_blobs(
-    n_samples=1000, centers=2, n_features=2, random_state=0, cluster_std=5.0
+    n_samples=1000,
+    centers=n_classes,
+    n_features=n_classes,
+    random_state=0,
+    cluster_std=5.0,
 )
 
 data = np.hstack((X, y[:, np.newaxis]))
@@ -19,18 +26,20 @@ train, test = np.split(data, [int(split_rate * (data.shape[0]))])
 
 X_train = train[:, :-1]
 y_train = train[:, -1]
+print("len(train):", len(y_train))
 
 X_test = test[:, :-1]
 y_test = test[:, -1]
+print("len(test):", len(y_test))
 
 
 class KNN:
-    def __init__(self, X_train, y_train, n_neighbors=5, weights="uniform"):
+    def __init__(self, X_train, y_train, n_neighbors=5, weights="uniform", n_classes=2):
         self.X_train = X_train
         self.y_train = y_train
         self.n_neighbors = n_neighbors
         self.weights = weights
-        self.n_classes = 3
+        self.n_classes = n_classes
 
     def euclidean_distance(self, a: np.array, b: np.array):
         return np.sqrt(np.sum((a - b) ** 2, axis=1))
@@ -43,37 +52,65 @@ class KNN:
             self.euclidean_distance(x_test, self.X_train) for x_test in X_test
         ]
 
-        print("\npoint_dist: \n", point_dist)
+        print("\npoint_dist: \n", len(point_dist[0]), len(point_dist))
 
         for row in point_dist:
             enum_neigh = enumerate(row)
 
             sorted_neigh = sorted(enum_neigh, key=lambda x: x[1])[: self.n_neighbors]
-            #  print("\nsorted_neigh: \n", sorted_neigh)
 
             ind_list = [tup[0] for tup in sorted_neigh]
             dist_list = [tup[1] for tup in sorted_neigh]
             dist.append(dist_list)
             neigh_ind.append(ind_list)
 
-        print("\ndist: \n", dist)
-        print("\nneigh_ind: \n", neigh_ind)
+        print("\ndist: \n", len(dist[0]), len(dist))
+        print("\nneigh_ind: \n", len(dist[0]), len(dist))
+        if return_distance:
+            return np.array(dist), np.array(neigh_ind)
 
         return np.array(neigh_ind)
 
     def predict(self, X_test):
-        neighbors = self.kneighbors(X_test)
-        print("\nneighbors\n", neighbors)
+        if self.weights == "uniform":
+            neighbors = self.kneighbors(X_test)
 
-        y_hat = np.array(
-            [
-                np.argmax(np.bincount(self.y_train[neighbor].astype(int)))
-                for neighbor in neighbors
-            ]
-        )
+            y_hat = np.array(
+                [
+                    np.argmax(np.bincount(self.y_train[neighbor].astype(int)))
+                    for neighbor in neighbors
+                ]
+            )
 
-        print("\ny_hat\n", y_hat)
-        return y_hat
+            print("\ny_hat\n", len(y_hat))
+            return y_hat
+        if self.weights == "distance":
+            dist, neigh_ind = self.kneighbors(X_test, return_distance=True)
+
+            inv_dist = 1 / dist
+            mean_inv_dist = inv_dist / np.sum(inv_dist, axis=1)[:, np.newaxis]
+
+            proba = []
+
+            for i, row in enumerate(mean_inv_dist):
+                row_pred = self.y_train[neigh_ind[i]]
+
+                for k in range(self.n_classes):
+                    indicies = np.where(row_pred == k)
+                    prob_ind = np.sum(row[indicies])
+                    proba.append(np.array(prob_ind))
+
+            predict_proba = np.array(proba).reshape(X_test.shape[0], self.n_classes)
+
+            y_hat = np.array([np.argmax(item) for item in predict_proba])
+
+            return y_hat
+        else:
+            exit(
+                NameError(
+                    "ERROR: Use 'uniform' or 'distance' for weights in KNN initialization"
+                )
+            )
 
     def score(self, X_test, y_test):
         y_pred = self.predict(X_test)
@@ -105,18 +142,11 @@ def plot(model: KNN, X_test: np.array, y_test: np.array, grid_step=0.2):
     plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cmap_bold)
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
-    plt.title("2-Class classification (k = %i)" % (21))
+    plt.title("2-Class classification (k = %i)" % (n_neighbors))
     plt.show()
 
 
-knn = KNN(X_train, y_train, n_neighbors=21)
+knn = KNN(X_train, y_train, n_neighbors=n_neighbors, weights="distance")
 plot(knn, X_test, y_test)
 score = knn.score(X_test, y_test)
 print(score)
-
-
-#  a = np.array([1, 1, 1, 0])
-#  b = np.array([[1, 1, 1, 1], [1, 1, 1, 0]])
-
-#  print(euclidean_distance(a, b))
-#  assert np.all(euclidean_distance(a, b) == np.array([1.0, 0.0]))
